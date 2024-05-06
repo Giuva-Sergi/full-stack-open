2359b981a495233f26b53ef8e48be9c2d2271e58
Giovanni Sergi <giovanni.srg@gmail.com>
2024-05-06 11:57:39 +0200

    unicafe step-1 completed

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -6,7 +6,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  function handleClick(name) {
+    switch (name) {
+      case "good":
+        return setGood(good + 1);
+      case "neutral":
+        return setNeutral(neutral + 1);
+      case "bad":
+        return setBad(bad + 1);
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button name="good" handler={() => handleClick("good")} />
+      <Button name="neutral" handler={() => handleClick("neutral")} />
+      <Button name="bad" handler={() => handleClick("bad")} />
+      <h2>statistics</h2>
+      <div>
+        <span>good</span>
+        <span>{good}</span>
+      </div>
+      <div>
+        <span>neutral</span>
+        <span>{neutral}</span>
+      </div>
+      <div>
+        <span>bad</span>
+        <span>{bad}</span>
+      </div>
+    </div>
+  );
 };
+
+function Button({ name, handler }) {
+  return <button onClick={handler}>{name}</button>;
+}
 
 export default App;
