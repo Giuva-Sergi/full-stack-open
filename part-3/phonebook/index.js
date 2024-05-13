d737d7303fa805df897dc4ea3bb2b9a2618c7170
Giovanni Sergi <giovanni.srg@gmail.com>
2024-05-13 17:00:53 +0200

    3.1 / 3.2 completed

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -1,8 +1,43 @@
 const express = require("express");
 const app = express();
 
+let contacts = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("<h1>Helloooooo</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(contacts);
+});
+
+app.get("/info", (req, res) => {
+  const now = new Date();
+  console.log(now.getUTCDay());
+  res.send(`<p>Phonebook has info for ${contacts.length} people</p>
+  <p>${now}</p>
+  `);
 });
 
 const PORT = 3001;
