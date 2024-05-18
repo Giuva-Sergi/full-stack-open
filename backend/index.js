edf63505e3f7debe3cacbf19b41dddeac966633f
Giovanni Sergi <giovanni.srg@gmail.com>
2024-05-18 10:07:55 +0200

    exported module for mongo connection

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,9 @@
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 const app = express();
+const Note = require("./models/note");
 
 app.use(express.json());
 app.use(cors());
@@ -30,17 +14,11 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => response.json(notes));
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404);
-  }
+  Note.findById(req.params.id).then((note) => res.json(note));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -50,16 +28,12 @@ app.post("/api/notes", (req, res) => {
     return res.status(400).json({ error: "Content missing" });
   }
 
-  const note = {
-    id: Math.random() * 10,
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-  };
+  });
 
-  notes = [...notes, note];
-  console.log(note);
-
-  res.json(note);
+  note.save().then((savedNote) => res.json(savedNote));
 });
 
 app.delete("/api/notes/:id", (req, res) => {
