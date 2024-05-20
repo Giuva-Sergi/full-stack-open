d1c9159af247bdeb748d02e5f5ced7b572d00528
Giovanni Sergi <giovanni.srg@gmail.com>
2024-05-20 12:20:30 +0200

    3.20 completed

--- part-3/phonebook/models/contact.js
+++ part-3/phonebook/models/contact.js
@@ -18,7 +18,16 @@ const contactSchema = new mongoose.Schema({
     type: String,
     minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 contactSchema.set("toJSON", {
