8b6e84345b99940aa418ff53b2e1ed662c9c0843
Giovanni Sergi <giovanni.srg@gmail.com>
2024-05-31 23:06:42 +0200

    ex 5.7 completed

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 function Blog({ blog }) {
+  const { title, author, url, likes } = blog;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  console.log(likes);
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div
+      style={{
+        border: "2px solid black",
+        padding: "0.5rem",
+        borderRadius: "7px",
+        marginBlock: "0.5rem",
+      }}
+    >
+      <p>
+        {blog.title}{" "}
+        <span>
+          <button onClick={() => setIsExpanded(!isExpanded)}>
+            {isExpanded ? "view" : "hide"}
+          </button>
+        </span>
+      </p>
+      {isExpanded && (
+        <>
+          <div>
+            <p>{url}</p>
+            <p>likes {likes}</p>
+            <p>{author}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
