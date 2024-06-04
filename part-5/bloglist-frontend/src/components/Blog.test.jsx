845193e4ba9a4fdccebcef7f7d2ccf526ae183d7
Giovanni Sergi <giovanni.srg@gmail.com>
2024-06-04 10:47:23 +0200

    ex 5.14 completed

--- part-5/bloglist-frontend/src/components/Blog.test.jsx
+++ part-5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import { expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("by default Blog renders only the author and title", async () => {
+test("by default Blog renders only the author and title", () => {
   const blog = {
     author: "Test author",
     title: "Test title",
@@ -26,4 +27,38 @@ test("by default Blog renders only the author and title", async () => {
   expect(titleEl).not.toBeNull();
   expect(urlEl).toBeNull();
   expect(likesEl).toBeNull();
+});
+
+test("url and likes are shown only if button is clicked", async () => {
+  const blog = {
+    author: "Test author",
+    title: "Test title",
+    url: "https://test.com",
+    user: {
+      username: "testUsername",
+      name: "Test name",
+    },
+  };
+
+  const { container } = render(<Blog blog={blog} />);
+  const blogEl = container.querySelector(".blog");
+
+  const button = screen.getByText("view");
+
+  const urlEl = blogEl.querySelector(".url");
+  const likesEl = blogEl.querySelector(".likes");
+
+  // before clicking the button elements are null
+  expect(urlEl).toBeNull();
+  expect(likesEl).toBeNull();
+
+  screen.debug(likesEl);
+
+  const user = userEvent.setup();
+
+  await user.click(button);
+
+  // after clicking elements are not null
+  expect(urlEl).toBeDefined();
+  expect(likesEl).toBeDefined();
 });
