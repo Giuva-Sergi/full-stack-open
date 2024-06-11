f7e2fa980acf5ef1597b98f8392ac194ba5d21bf
Giovanni Sergi <giovanni.srg@gmail.com>
2024-06-11 17:43:54 +0200

    fixed minor bug

--- part-4/blog/controllers/tests.js
+++ part-4/blog/controllers/tests.js
@@ -2,11 +2,11 @@ const testRouter = require("express").Router();
 const User = require("../models/user");
 const Blog = require("../models/blog");
 
-testRouter.post("/reset", await (request, response) => {
-    await User.deleteMany({});
-    await Blog.deleteMany({});
+testRouter.post("/reset", async (request, response) => {
+  await User.deleteMany({});
+  await Blog.deleteMany({});
 
-    response.status(204).end();
-})
+  response.status(204).end();
+});
 
 module.exports = testRouter;
