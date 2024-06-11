83a5ba1258006ee40167b4762a9b076f73e73e5e
Giovanni Sergi <giovanni.srg@gmail.com>
2024-06-11 17:42:27 +0200

    added controller for testing

--- part-4/blog/app.js
+++ part-4/blog/app.js
@@ -6,6 +6,7 @@ const info = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const testRouter = require("./controllers/tests");
 const app = express();
 const middleware = require("./utils/middleware");
 
@@ -18,6 +19,11 @@ app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/tests", testRouter);
+}
+
 app.use(middleware.errorHandler);
 
 module.exports = app;
