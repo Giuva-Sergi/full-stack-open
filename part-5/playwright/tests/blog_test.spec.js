617a570467a8e812bfc914f63c8d4c281d88b722
Giovanni Sergi <giovanni.srg@gmail.com>
2024-06-13 09:57:02 +0200

    ex 5.18 completed

--- part-5/playwright/tests/blog_test.spec.js
+++ part-5/playwright/tests/blog_test.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const loginWith = require("./helper");
 
 describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
@@ -22,11 +23,27 @@ describe("Blog app", () => {
 
   describe("login", () => {
     test("succeeds with correct credentials", async ({ page }) => {
-      await page.getByTestId("username").fill("testUser");
-      await page.getByTestId("password").fill("testPassword");
-      await page.getByRole("button", { name: "login" }).click();
+      await loginWith(page, "testUser", "testPassword");
 
       await expect(page.getByText("Test User logged in")).toBeVisible();
+    });
+
+    test("login fails with wrong credentials", async ({ page }) => {
+      await loginWith(page, "testUser", "wrongPassword");
+
+      await expect(
+        page.getByText("invalid username or password")
+      ).toBeVisible();
+
+      await expect(page.getByText("invalid username or password")).toHaveCSS(
+        "border",
+        "5px solid rgb(255, 0, 0)"
+      );
+
+      await expect(page.getByText("invalid username or password")).toHaveCSS(
+        "background-color",
+        "rgba(244, 24, 24, 0.3)"
+      );
     });
   });
 });
