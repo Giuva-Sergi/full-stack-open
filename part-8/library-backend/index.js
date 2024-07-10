2cdecbeabfcdd1267bb12e99bad99be8181090e2
Giovanni Sergi <giovanni.srg@gmail.com>
2024-07-10 11:14:00 +0200

    ex 8.2 completed

--- part-8/library-backend/index.js
+++ part-8/library-backend/index.js
@@ -98,9 +98,19 @@ let books = [
 */
 
 const typeDefs = `
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+    
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -108,6 +118,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      return books;
+    },
   },
 };
 
