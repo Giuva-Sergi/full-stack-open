78bcd36f635346d646c7444a90bfee628109c8e8
Giovanni Sergi <giovanni.srg@gmail.com>
2024-09-02 13:14:11 +0200

    ex 9.2 completed

---
+++ part-9/exerciseCalculator.ts
@@ -0,0 +1,52 @@
+type Success = 1 | 2 | 3;
+
+interface SummaryResult {
+  periodLength: number;
+  numberOfTrainingDays: number;
+  target: number;
+  avgTime: number;
+  success: boolean;
+  rating: Success;
+  ratingDescription: string;
+}
+
+const calculateExercise = (
+  dailyExerciseHours: number[],
+  targetAmount: number
+) => {
+  let success = false;
+  let rating;
+  let ratingDescription;
+  const numberOfDays = dailyExerciseHours.length;
+  const numberOfTrainingDays = dailyExerciseHours.filter(
+    (day) => day !== 0
+  ).length;
+  const arraySum = dailyExerciseHours.reduce((acc, currVal): number => {
+    return acc + currVal;
+  }, 0);
+  const avgTime = arraySum / dailyExerciseHours.length;
+
+  if (avgTime > targetAmount) {
+    success = true;
+    rating = 3;
+    ratingDescription = "Very good!";
+  } else if (avgTime < targetAmount && targetAmount - avgTime <= 0.5) {
+    rating = 2;
+    ratingDescription = "not too bad but could be better";
+  } else {
+    rating = 1;
+    ratingDescription = "you should work harder";
+  }
+
+  return {
+    periodLength: numberOfDays,
+    numberOfTrainingDays,
+    target: targetAmount,
+    avgTime,
+    success,
+    rating,
+    ratingDescription,
+  };
+};
+
+console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 4));
